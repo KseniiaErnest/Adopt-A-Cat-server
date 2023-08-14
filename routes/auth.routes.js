73868ba772c42ff256ff4ff2dbfa2e6e3ce34145
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-08-14 15:09:33 -0400

    Update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -129,10 +129,13 @@ router.get('/verify', isAuthenticated, (req, res, next) => {
   // If JWT token is valid the payload gets decoded by the
   // isAuthenticated middleware and made available on `req.payload`
   console.log(`req.payload`, req.payload);
+  console.log({userOne: req.user})
  
   // Send back the object with user data
   // previously set as the token payload
   res.status(200).json(req.payload);
+  // res.status(200).json({ user: req.user});
+
 });
 
 // Update user profile
