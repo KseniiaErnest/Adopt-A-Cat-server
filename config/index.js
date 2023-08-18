6776ccc8b74c93ad6a135078d8f7516b8e6f7ebe
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-08-17 21:35:34 -0400

    updated cors

--- config/index.js
+++ config/index.js
@@ -22,14 +22,15 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  // app.use(
-  //   cors({
-  //     credentials: true,
-  //     origin: process.env.ORIGIN || 'http://localhost:3000'
-  //   })
-  // );
+  app.use(
+    cors({
+      credentials: true,
+      // origin: process.env.ORIGIN || 'http://localhost:3000'
+      origin: true
+    })
+  );
 
-  app.use(cors(true));
+  // app.use(cors(true));
 
   // In development environment the app logs
   app.use(logger("dev"));
